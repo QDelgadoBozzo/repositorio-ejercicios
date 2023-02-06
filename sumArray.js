4f358374685cd64e64451d7ea6817d03768096e6
QDelgadoBozzo <quelvin.delgadob@gmail.com>
2023-02-06 00:05:55 -0500

    12321321

--- sumArray.js
+++ sumArray.js
@@ -1,5 +1,6 @@
 
 function sumArray(array,n){
+    var n
 let sumas=[]
 for(let i=0;i<array.length;i++){
     for(let j=0; j<array.length;j++){
@@ -8,13 +9,13 @@ if(i!==j) sumas.push(array[i]+array[j])
 return sumas.includes(n)
 
 }
+// console.log(sumArray([2,5,7,10,11,15,20],1))
+
+//muestra el resultado de sum array
+//console.log(sumArray([2,5,7,10,11,15,20],1))
 
 
-//mostrar el resultado de esPrimo 
-//console.log(esPrimo(6))
 
-//mostrar el resultado en consola
-// console.log(mismaCantidadCaracteres(['hi','hello','ni hao','guten tag'],2))
 
 
 
