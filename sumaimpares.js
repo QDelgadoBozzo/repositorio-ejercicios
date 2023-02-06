0732d3a0f31c5e1b57925571a0e5dabc073faad0
QDelgadoBozzo <quelvin.delgadob@gmail.com>
2023-02-05 21:14:48 -0500

    2

--- sumaimpares.js
+++ sumaimpares.js
@@ -1,2 +1,25 @@
-//suma impares de un array
-var a
+//suma impares de un array;
+function esImpar(numero){
+    var numero
+    if(numero%2===0){
+        return false
+    }
+    return true
+}
+function sumaTodosImpares(array){
+    let acumulador =[]
+let array
+    array=[1,2,3,4,5,6]
+       
+    
+    for (let i=0;i< array.length;i++){
+        if (esImpar(array[i])) {
+            acumulador+= array[i]
+        }
+    return false
+    }
+
+// console.log(sumaTodosImpares(array))
+console.log(esImpar(5))
+
+}
